79a2a54ecbce21d5cc321866672835fad90e5157
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-12 13:30:00 +0200

    Add: getPost()

--- src/main/java/com/github/dickens/blogapp/MyRestController.java
+++ src/main/java/com/github/dickens/blogapp/MyRestController.java
@@ -1,24 +1,37 @@
 package com.github.dickens.blogapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
+import java.util.Optional;
 
 @RestController
 public class MyRestController {
 
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    PostRepository postRepository;
 
     /*
     curl -H"Content-Type: application/json" -X POST -d {\"userName\":\"mkyong\",\"password\":\"abc\"} http://localhost:8080/user/add
      */
     @PostMapping(value = "/user/add")
     public void addUser(@RequestBody User user) {
+        System.out.println("Post");
         userRepository.save(user);
+    }
+
+    /*
+    curl -i GET http://localhost:8080/posts/4
+    */
+    @GetMapping(value = "/posts/{postId}")
+    public Optional<Post> getPost(@PathVariable int postId) {
+        System.out.println("GET");
+        Optional<Post> post = postRepository.findById(postId);
+        System.out.println(post);
+        return post;
     }
 
 }
