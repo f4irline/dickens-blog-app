1e3f440871fa5f5652d135ce9d7e57a696fc64f2
Tommi Lepola <lepola.tommi@gmail.com>
2019-03-12 09:34:46 +0200

    Fix Spring main class name

--- src/main/java/com/github/dickens/blogapp/BlogApp.java
+++ src/main/java/com/github/dickens/blogapp/BlogApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogAppApplication {
+public class BlogApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogAppApplication.class, args);
+		SpringApplication.run(BlogApp.class, args);
 	}
 
 }
