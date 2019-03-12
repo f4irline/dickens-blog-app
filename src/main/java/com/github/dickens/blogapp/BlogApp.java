5254179a489d48f8ab9c55c034b72098a8efa2a5
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-12 12:27:57 +0200

    Add: Utils.java that contains hashMyPassword()

--- src/main/java/com/github/dickens/blogapp/BlogApp.java
+++ src/main/java/com/github/dickens/blogapp/BlogApp.java
@@ -1,13 +1,22 @@
 package com.github.dickens.blogapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogApp {
+public class BlogApp implements CommandLineRunner {
+
+	@Autowired
+	UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApp.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.save(new User("HerskaHeikki","Passu"));
+	}
 }
