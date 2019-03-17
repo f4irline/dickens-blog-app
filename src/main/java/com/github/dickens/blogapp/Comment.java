851fe036b90fc6ef08efb9b08b6b1f4dd4398603
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-17 17:03:45 +0200

    Add: variables/Getter&Setters/Constructor to Comment class

--- src/main/java/com/github/dickens/blogapp/Comment.java
+++ src/main/java/com/github/dickens/blogapp/Comment.java
@@ -1,4 +1,63 @@
 package com.github.dickens.blogapp;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
 public class Comment {
+
+    @Id
+    @GeneratedValue
+    private int commentId;
+    @OneToOne
+    @JoinColumn(name ="POST_ID")
+    private Post post;
+    @OneToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+    private String body;
+    private int likes;
+
+    public Comment(Post post, User user, String body) {
+        this.post = post;
+        this.user = user;
+        this.body = body;
+    }
+
+    public int getCommentId() {
+        return commentId;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
 }
