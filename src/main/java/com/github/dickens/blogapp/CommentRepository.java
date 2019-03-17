9a6a42370871b4b40540485baa97d1c76fad4a92
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-17 17:38:53 +0200

    Add: findCommentsByPost()

--- src/main/java/com/github/dickens/blogapp/CommentRepository.java
+++ src/main/java/com/github/dickens/blogapp/CommentRepository.java
@@ -3,4 +3,5 @@ package com.github.dickens.blogapp;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CommentRepository extends CrudRepository<Comment, Integer> {
+    public Iterable<Comment> findCommentsByPost(Post post);
 }
