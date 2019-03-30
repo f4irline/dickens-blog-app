919b2886d7af0e473387660cead8281dcf8510b6
Tommi Lepola <lepola.tommi@gmail.com>
2019-03-30 23:56:27 +0200

    Try to fix HTTP issues in front end.

--- src/main/java/com/github/dickens/blogapp/security/SecurityConfig.java
+++ src/main/java/com/github/dickens/blogapp/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.github.dickens.blogapp.user.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.BeanIds;
@@ -67,6 +68,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
             .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/api/**").permitAll()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/static/**").permitAll()
                 .antMatchers("/**").permitAll()
