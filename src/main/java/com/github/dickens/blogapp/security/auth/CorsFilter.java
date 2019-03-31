ecf30896bbe6a4511cdd3967adee00819ccf10aa
Tommi Lepola <lepola.tommi@gmail.com>
2019-03-31 14:55:42 +0300

    Backend: CorsFilter: doFilterInternal(): Implement checking for OPTIONS status response.

---
+++ src/main/java/com/github/dickens/blogapp/security/auth/CorsFilter.java
@@ -0,0 +1,23 @@
+package com.github.dickens.blogapp.security.auth;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CorsFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        if ("OPTIONS".equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
