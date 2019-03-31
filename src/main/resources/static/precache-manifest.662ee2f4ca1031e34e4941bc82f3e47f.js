6e4a69be172774cb567b946d16e3b37452f6449e
Tommi Lepola <lepola.tommi@gmail.com>
2019-03-31 14:36:23 +0300

    Spring: AuthController: registerUser(): Refactor badRequest response.
    Front: Fix validation for user role.

---
+++ src/main/resources/static/precache-manifest.662ee2f4ca1031e34e4941bc82f3e47f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9adfc3a6172e54e068458beecd062d0e",
+    "url": "/static/media/header_placeholder.9adfc3a6.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a54f775c20c7644f1d6b",
+    "url": "/static/js/main.ae334f78.chunk.js"
+  },
+  {
+    "revision": "d6ecd72f4292440ce31c",
+    "url": "/static/js/2.c16a2518.chunk.js"
+  },
+  {
+    "revision": "a54f775c20c7644f1d6b",
+    "url": "/static/css/main.656edc76.chunk.css"
+  },
+  {
+    "revision": "d6ecd72f4292440ce31c",
+    "url": "/static/css/2.8e69701c.chunk.css"
+  },
+  {
+    "revision": "f39d8bdd4282513644519ee7fd8b95a8",
+    "url": "/index.html"
+  }
+];
