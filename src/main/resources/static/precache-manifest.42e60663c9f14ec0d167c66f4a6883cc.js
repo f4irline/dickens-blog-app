702c6751b9c6e8ca130a7ac2f5ff1771747ee6b1
Tommi Lepola <lepola.tommi@gmail.com>
2019-03-31 11:38:47 +0300

    Try to fix HTTP issues in front end.

---
+++ src/main/resources/static/precache-manifest.42e60663c9f14ec0d167c66f4a6883cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9adfc3a6172e54e068458beecd062d0e",
+    "url": "/static/media/header_placeholder.9adfc3a6.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "54a2dfa7bc07924ba863",
+    "url": "/static/js/main.001490c1.chunk.js"
+  },
+  {
+    "revision": "d6ecd72f4292440ce31c",
+    "url": "/static/js/2.c16a2518.chunk.js"
+  },
+  {
+    "revision": "54a2dfa7bc07924ba863",
+    "url": "/static/css/main.656edc76.chunk.css"
+  },
+  {
+    "revision": "d6ecd72f4292440ce31c",
+    "url": "/static/css/2.8e69701c.chunk.css"
+  },
+  {
+    "revision": "10ec139b7bf7e22d2d4dd7db28a46a63",
+    "url": "/index.html"
+  }
+];
