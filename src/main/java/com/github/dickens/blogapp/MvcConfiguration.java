cfeb2d995dea40b84132cb90fcab9e0d8ee96725
Tommi Lepola <lepola.tommi@gmail.com>
2019-03-31 15:25:59 +0300

    Add content-length to allowed headers.

--- src/main/java/com/github/dickens/blogapp/MvcConfiguration.java
+++ src/main/java/com/github/dickens/blogapp/MvcConfiguration.java
@@ -23,9 +23,9 @@ public class MvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://localhost:3000", "http://localhost:3000", "https://dickens-blog-app.herokuapp.com")
                 .allowedMethods("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH")
-                .allowedHeaders("authorization, content-type, xsrf-token")
+                .allowedHeaders("authorization, content-type, content-length, xsrf-token")
                 .exposedHeaders("xsrf-token")
                 .maxAge(3600);
     }
