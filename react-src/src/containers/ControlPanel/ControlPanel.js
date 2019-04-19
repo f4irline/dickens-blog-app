7492b5f1a338a124653864ee4fb40c63c5823209
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-04-19 19:43:06 +0300

    Add: ControlPanel with users

---
+++ react-src/src/containers/ControlPanel/ControlPanel.js
@@ -0,0 +1,123 @@
+import React, {Component} from 'react';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import axios from '../../axios-instance';
+
+
+class ControlPanel extends Component {
+
+  state = {
+    showDialog: false,
+    users: [],
+  }
+
+  componentDidMount() {
+    this.updateUsers();
+  }
+
+  updateUsers() {
+    axios.get('users/all').then((res) =>  this.setState({users: res.data})).catch(err => console.log(err));
+  }
+
+  handleSelectChange(event, user) {
+    let isAdmin;
+    if(event.target.value === 'Admin') {
+      isAdmin = true;
+    } else if(event.target.value === 'User') {
+      isAdmin = false;
+    }
+    axios.post(`users/role/${user.userId}/${isAdmin}`).then(() => this.updateUsers()).catch(err => console.log(err));
+  }
+
+  handleDeleteOnClick(event, user) {
+    
+  }
+
+  createData(user) {
+    let userId = user.userId;
+    let userName = user.userName;
+    let wholeName = user.wholeName;
+    let role = '';
+    if(user.roles.length > 1) {
+      role = 'Admin'
+    } else {
+      role = 'User'
+    }
+    console.log(userId + userName + wholeName + role)
+    return {userId, userName, wholeName, role};
+  }
+
+  render() {
+    const styles = theme => ({
+      root: {
+        width: '100%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto',
+      },
+      table: {
+        minWidth: 700,
+      },
+    });
+    const rows = this.state.users.map(data => this.createData(data))
+
+    const options = [
+      { value: 'yes', label: 'Yes' },
+      { value: 'no', label: 'No' },
+    ]
+    
+    return (
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align='right'>User id</TableCell>
+              <TableCell align='right'>Username</TableCell>
+              <TableCell align='right'>Name</TableCell>
+              <TableCell align='right'>Role</TableCell>
+              <TableCell align='right'>Delete</TableCell>
+            </TableRow>
+          </TableHead>
+            <TableBody>
+              {rows.map(row => (
+                <TableRow key={row.userId}>
+                  <TableCell component='th' scope='row'>
+                    {row.userId}
+                  </TableCell>
+                  <TableCell align='right'>{row.userName}</TableCell>
+                  <TableCell align='right'>{row.wholeName}</TableCell>
+                  <TableCell align='right'>
+                    <Select value={row.role} onChange={(event) => {
+                      this.handleSelectChange(event, row);
+                    }}>
+                      <MenuItem value='Admin'>Admin</MenuItem>
+                      <MenuItem value='User'>User</MenuItem>
+                    </Select>
+                  </TableCell>
+                  <TableCell align='right'>
+                    <IconButton onClick={(event) => {
+                      this.handleDeleteOnClick(event, row);
+                    }}>
+                      <DeleteIcon/>
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      );
+
+    }
+  
+}
+
+export default ControlPanel;
+
