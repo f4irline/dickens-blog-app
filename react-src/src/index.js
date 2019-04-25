cf96a140370129e788ae6f742b514cd8c0107517
Tommi Lepola <lepola.tommi@gmail.com>
2019-04-25 19:56:44 +0300

    Add https redirect package

--- react-src/src/index.js
+++ react-src/src/index.js
@@ -4,8 +4,14 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import HttpsRedirect from 'react-https-redirect';
 
-const app = <BrowserRouter><App /></BrowserRouter>;
+const app = 
+<HttpsRedirect>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+</HttpsRedirect>;
 
 ReactDOM.render(app, document.getElementById('root'));
 
